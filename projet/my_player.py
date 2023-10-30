2237a894c9f2ec0d8e0f7b7869f88118181e2572
thaisgns <144276725+thaisgns@users.noreply.github.com>
2023-10-30 18:45:51 -0400

    Add files via upload
    my_player from Dossier GitHub Thaïs

--- projet/my_player.py
+++ projet/my_player.py
@@ -36,71 +36,80 @@ class MyPlayer(PlayerAbalone):
         Returns:
             Action: selected feasible action
         """
-        self.h_alphabeta_search(current_state)
+        return self.h_alphabeta_search(current_state)
         raise MethodNotImplementedError()
 
     def cutoff_depth(self,depth,d):
         """A cutoff function that searches to depth d."""
         return depth > d
 
-    def h_alphabeta_search(self, current_state, h=lambda s, p: 0):
+    def heuristic(current_state):
+        dico = dict()
+        players = current_state.players
+        dico[players[0].get_id()] = 0
+        dico[players[1].get_id()] = 0
+        return dico
+
+    def h_alphabeta_search(self, current_state, h=heuristic):
         """Search game to determine best action; use alpha-beta pruning.
         As in [Figure 5.7], this version searches all the way to the leaves."""
 
         infinity = math.inf
         player = current_state.get_next_player()
         print(player.get_id())
-        
+       
         def max_value(current_state, alpha, beta,depth):
+            print("depth max =", depth)
             if current_state.is_done():
                 return current_state.compute_scores(player.get_id()), None
-            print(self.cutoff_depth(depth,20))
-            if self.cutoff_depth(depth,20):
-                print("Incrémentation depth=",depth)
-                print("ici")
-                dico = dict()
-                dico[0] = 0
-                dico[player.get_id()] = 4
-                return dico, None
+            print('on peut entrer dans le cutoff max ? ', self.cutoff_depth(depth, 4))
+            if self.cutoff_depth(depth, 4):
+                print("DANS LE CUTOFF DU MAX VALUE")
+                return h(current_state), None
             v, move = -infinity, None
             v_dict = dict()
             for a in current_state.generate_possible_actions():
-                print(min_value(a.get_next_game_state(), alpha, beta,depth+1))
-                v2, _ = min_value(a.get_next_game_state(), alpha, beta,depth+1)  
+                print("DANS LA BOUCLE FOR MAX VALUE")
+                v2, _ = min_value(a.get_next_game_state(), alpha, beta, depth+1)  
                 #v_dict=v2
-                print(v2)
+                print("v2 dans max value =",v2)
                 score = v2[player.get_id()]
                 if score > v:
+                    print("Cas score > v dans max value : ")
                     v, move = score, a
                     v_dict = v2
                     alpha = max(alpha, score)
                 if v >= beta:
+                    print( "Cas v >= beta : dans max value")
                     return v_dict, move
             return v_dict, move
 
         def min_value(current_state, alpha, beta,depth):
+            print("depth min =", depth)
             if current_state.is_done():
                 return current_state.compute_scores(player.get_id()), None
-            if self.cutoff_depth(depth,20):
-                print("Incrémentation depth=",depth)
-                dico = dict()
-                dico[0] = 0
-                dico[player.get_id()] = 4
-                return dico, None
+            print('on peut entrer dans le cutoff min ? ', self.cutoff_depth(depth, 4))
+            if self.cutoff_depth(depth,4):
+                print("DANS LE CUTOFF DU MIN VALUE")
+                print("resultat de h : ", h(current_state))
+                return h(current_state), None
             v, move = +infinity, None
             v_dict = dict()
             for a in current_state.generate_possible_actions():
-                v2,_ = max_value(a.get_next_game_state(), alpha, beta,depth+1)
+                print("DANS LA BOUCLE FOR MIN VALUE")
+                v2,_ = max_value(a.get_next_game_state(), alpha, beta, depth+1)
                 score = v2[player.get_id()]
                 if score < v:
+                    print("Cas score < v dans min : ")
                     v, move = score, a
                     v_dict = v2
                     beta = min(beta, v)
                 if v <= alpha:
+                    print( "Cas v <= alpha dans min : ")
                     return v_dict, move
             return v_dict, move
 
-        return max_value(current_state, -infinity, +infinity,0)
+        return max_value(current_state, -infinity, +infinity, 0)
         
 
     def alphabeta_search(self, current_state):
@@ -109,7 +118,6 @@ class MyPlayer(PlayerAbalone):
 
         infinity = math.inf
         player = current_state.get_next_player()
-        print(player.get_id())
         
         def max_value(current_state, alpha, beta):
             if current_state.is_done():
